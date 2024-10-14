39f1d0dcb05f08b2750a0c483f8dbfaea7334884
AtlasTheSeventh <borys.basinski@wp.pl>
2024-10-14 16:46:08 +0200

    first commit

---
+++ src/main/java/net/atlas/halfdirts/Halfdirts.java
@@ -0,0 +1,16 @@
+package net.atlas.halfdirts;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Halfdirts implements ModInitializer {
+	public static final String MOD_ID = "halfdirts";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/atlas/halfdirts/HalfdirtsClient.java
@@ -0,0 +1,10 @@
+package net.atlas.halfdirts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HalfdirtsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
