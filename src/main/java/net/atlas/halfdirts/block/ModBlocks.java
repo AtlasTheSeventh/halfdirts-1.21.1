f7758881a63465ed9aab875b1ed67bc803114fe1
AtlasTheSeventh <borys.basinski@wp.pl>
2024-10-16 19:45:27 +0200

    partly fixed dirt slabs

--- src/main/java/net/atlas/halfdirts/block/ModBlocks.java
+++ src/main/java/net/atlas/halfdirts/block/ModBlocks.java
@@ -2,16 +2,16 @@ package net.atlas.halfdirts.block;
 
 import net.atlas.halfdirts.Halfdirts;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 import static net.minecraft.block.Blocks.CAULDRON;
 
 public class ModBlocks {
-    public static final Block DIRT_SLAB = register("block_of_silly", new Block(FabricBlockSettings.copyOf(Blocks.DIRT)));
+    public static final Block DIRT_SLAB = register("dirt_slab", new Block(AbstractBlock.Settings.create().mapColor(MapColor.DIRT_BROWN).strength(0.5F).sounds(BlockSoundGroup.GRAVEL)));
 
     private static Block register(String name, Block block) {
         return Registry.register(Registries.BLOCK, Identifier.of(Halfdirts.MOD_ID, name), block);
@@ -21,4 +21,3 @@ public class ModBlocks {
         Halfdirts.LOGGER.info("Registering Blocks for " + Halfdirts.MOD_ID);
     }
 }
-
